d43637ad6faaff0bcdbb9b368eabb6b233d5a3b3
TomaszSorobka <tomek.sorobka@gmail.com>
2023-04-29 15:03:20 +0200

    restructured, introduced OOP design

---
+++ src/scrapers/scraper.py
@@ -0,0 +1,6 @@
+from abc import ABC
+
+class Scraper(ABC):
+
+    def __init__(self, driver):
+        self.driver = driver
---
+++ src/scrapers/scraper_mumsnet.py
@@ -0,0 +1,36 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from scrapers.scraper import Scraper
+
+
+class ScrapeMumsnet(Scraper):
+    def __init__(self, driver):
+        self.driver = driver
+
+    def getMain(self):
+        try:
+            main = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.TAG_NAME, 'main')))
+            
+            return main
+        except:
+            print("Error: could not get main from Mumsnet")
+
+    def getHeadlines(self):
+        main = self.getMain()
+        headlinesText = []
+        headlines = main.find_elements(By.CSS_SELECTOR, '[data-click-id*="topic-thread-"]')
+        for headline in headlines:
+            headlinesText.append(headline.text) 
+        return headlinesText
+    
+    def scrapeData(self):
+        link = 'https://www.mumsnet.com/talk/extra_curricular_activities?order-by=newest&page='
+        allHeadlines = []
+        for i in range(1,2):
+            self.driver.get(link + str(i))
+            allHeadlines += self.getHeadlines()
+            print(type(allHeadlines))
+        return allHeadlines
