7fb3e756395614b87600ca8edc51321d4c7a0e6c
TomaszSorobka <tomek.sorobka@gmail.com>
2023-04-29 11:42:10 +0200

    get headlines from parenting forum

--- scraper-selenium.py
+++ scraper-selenium.py
@@ -1,13 +1,26 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 
-driver = webdriver.Chrome(PATH)
+options = webdriver.ChromeOptions()
+options.add_argument('--ignore-certificate-errors')
+options.add_argument('--ignore-ssl-errors')
+driver = webdriver.Chrome(PATH, chrome_options=options)
 
-driver.get("https://www.mumsnet.com/talk/extra_curricular_activities?page=3&order-by=newest")
-main = driver.find_element(By.TAG_NAME, 'main')
-print(main.text)
+driver.get("https://www.mumsnet.com/talk/extra_curricular_activities?page=1&order-by=newest")
 
-driver.quit()
+try:
+    main = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.TAG_NAME, 'main'))
+    )
+
+    headlines = main.find_elements(By.CSS_SELECTOR, '[data-click-id="topic-thread-1"]')
+    for head in headlines:
+        print(head.text)
+    
+finally:
+    driver.quit()
